92c5a819333273b8f5b7e1174d61cfcf25251872
Lucy Lee <gamesandhannah@gmail.com>
2021-03-13 10:53:40 -0500

    Added favicon for all HTML pages, minor indenting and folder fixes; added on Game.py for lab 11; updated relative links in README.md

--- README.md
+++ README.md
@@ -4,37 +4,37 @@
 ##### Contributor: Lucy Lee
 
 All my assignments done at this micro-credential can be found here.
-- Lab 1
-  - Create a website with basic HTML using different heading tags.
-- Lab 2
-  - Adding onto lab 1, insert an image.
-- Lab 3
-  - Adding onto lab 2, insert hyperlinks and ordered/unordered lists.
-- Lab 4
-  - Adding onto lab 3, 
-- Lab 5
-  - Adding onto lab 4,
-- Lab 6
-  - Adding onto lab 5, create a Favicon.
-- Lab 7 (index.html)
+- [Index (Lab 7)](index.html)
   - Create an index page for all labs.
-- Lab 8
+- [Lab 1](lab1.html)
+  - Create a website with basic HTML using different heading tags.
+- [Lab 2](lab2.html)
+  - Adding onto lab 1, insert an image.
+- [Lab 3](lab3.html)
+  - Adding onto lab 2, insert hyperlinks and ordered/unordered lists.
+- [Lab 4](lab4.html)
+  - Adding onto lab 3,
+- [Lab 5](lab5.html)
+  - Adding onto lab 4,
+- [Lab 6](lab6)
+  - Adding onto lab 5, create a Favicon.
+- [Lab 8](lab8)
   - Practice with CSS using Lorem ipsum filler text.
-- Lab 9
+- [Lab 9](lab9)
   - Create a navigation bar using an external CSS sheet.
 - Lab 10
   - Create a form (Fortune Teller theme) using Javascript.
-- Lab 11
+- [Lab 11](Game.py)
   - Create simple games using Python.
-- Lab 12
+- [Lab 12](lab12)
   - Create prompts, alerts, and confirmation pop-up windows that ask the user to input a quadratic equation, and will calculate the volume of a cylinder using Javascript.
-- Lab 13
+- [Lab 13](lab13)
   - Create a calculator (either "age" or "lifetime supply" themed) using Javascript.
 - Lab 14
   - Write a function called "cash register" that takes a shopping cart object and returns the total price of the items in the shopping cart, using an array and for loop in Javascript.
 - Lab 15
   - Populate a Ninja object with user input using Javascript.
-- [Lab 16 (Group project)](Lab%2016%20(Group))
+- [Lab 16 (Group project)](lab16)
   - Fix a demo website.
 - Lab 17
   - Use jQuery to manipulate CSS and apply animation to a box.
---
+++ lab11/Game.py
@@ -0,0 +1,112 @@
+import random
+
+#Menu
+def tryAgain():
+        answer = input("\n====================\nDo you want to play another game? Y/N ")
+
+        #Invalid input check
+        while answer not in ('Y', 'N'):
+            print("That is not valid input. Please type either Y or N.")
+            answer = input("\nDo you want to play again? Y/N ")
+
+        #User says Y
+        if answer == "Y":
+            choice = int(input("\nWhich game would like you to play? Choose 1 or 2.\n1) Roll of the Dice\n2) Guess the Number\n\nChoice: "))
+
+            #Invalid input check
+            while choice not in (1, 2):
+                print("That is not a valid choice. Please type either 1 or 2.\n")
+                choice = int(input("Which game would like you to play? Choose 1 or 2.\n1) Roll of the Dice\n2) Guess the Number\n\nChoice: "))
+
+            if choice == 1:
+                #Game Choice 1
+                diceRoll()
+            else:
+                #Game Choice 2
+                guessNum()
+        
+        #User says N
+        else:
+            print("\nThank you for playing!")
+
+
+#Guessing Game - Roll of the Dice
+def diceRoll():
+        print("\n====================\nWelcome to the Roll of the Dice. You will have to guess a number from 1 to 6.")
+        
+        #Ask the user for # of rolls
+        rolls = int(input("How many rolls do you want to play? "))
+
+        points = 0
+
+        #For each roll, the user guesses a random # between 1-6
+        #Give the user +6 points if they are right
+        #Give the user -1 point if they are wrong
+    
+        while rolls > 0:
+            x = random.randint(1,6)
+            #print ("Correct answer: ", x) Check
+            guess = int(input("\nYour guess: "))
+
+            #Optional: Invalid input check (Guess should be 1-6)
+            
+            print("\nCorrect answer: ", x)
+
+            if guess == x:
+                points += 6
+                print("Great job! +6 points")
+            else:
+                points -= 1
+                print("Awww! Try again? -1 point")
+            rolls -= 1
+
+        #Print the final score after all the rolls are played
+        print("\nYour final score: ", points)
+
+        #Back to menu
+        tryAgain()
+
+#Guessing Game - Guess the Number
+def guessNum():
+    print("\n====================\nWelcome to Guess the Number.")
+    
+    answer = random.randint(1,100)
+
+    rolls = 1
+    print("Guess an integer from 1 to 100. You will have 5 tries. Go!")
+
+
+    while rolls < 6:
+        guess = int(input("Guess " + str(rolls) + ": "))
+
+        #Optional: Invalid input check (Guess should be 1-100)
+
+        if guess == answer:
+            print("Great job, you guessed it!")
+            rolls = 6
+        elif guess > answer:
+            print("Your guess is greater than the answer.")
+        else:
+            print("Your guess is lower than the answer.")
+
+        rolls += 1
+
+    #Back to menu
+    tryAgain()
+
+#Start program
+choice = int(input("\nWhich game would like you to play? Choose 1 or 2.\n1) Roll of the Dice\n2) Guess the Number\n3) EXIT\n\nChoice: "))
+
+while choice not in (1, 2, 3):
+        print("That is not valid input. Please type either 1, 2 or 3.")
+        choice = int(input("\nWhich game would like you to play? Choose 1 or 2.\n1) Roll of the Dice\n2) Guess the Number\n3) EXIT\n\nChoice: "))
+
+if choice == 1:
+    #Game Choice 1
+    diceRoll()
+elif choice == 2:
+    #Game Choice 2
+    guessNum()
+    #End program
+else:
+    print("Have a nice day!")
