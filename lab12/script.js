9959e192c18d1f11cda026b0f6c4b8f6ce20b05a
Lucy Lee <gamesandhannah@gmail.com>
2021-03-13 19:29:21 -0500

    Fixed Javascript for lab 12, added banner to index

--- lab12/script.js
+++ lab12/script.js
@@ -1,7 +1,25 @@
-var a, b, c, y, x1, x2;
-a = 1; b = 2; c = 2;
+// Quadratic Formula
+var x1, x2, discriminant;
 
-x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
-x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
 
-document.getElementById("quadForm").innerHTML = "The discriminant found by the quadratic formula is " + x1 + " or " + x2 + ".";
+discriminant = b ** 2 - 4 * a * c;
+
+if (discriminant > 0) {
+  x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+  x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+
+  console.log("Roots: " + x1 + ", " + x2);
+}
+else if (discriminant == 0) {
+  x1 = x2 = -b / (2 * a);
+
+  console.log("Root: " + x1 + ", " + x2);
+}
+else {
+  console.log("Roots are imaginary.");
+}
+
+document.getElementById("quadForm").innerHTML = "The roots found by the quadratic formula are " + x1 + " or " + x2 + ".";
